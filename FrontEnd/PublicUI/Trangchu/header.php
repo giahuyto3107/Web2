d10c9b0cd407780732a8cd8e86dbc0321d915113
Trankydai <kydai2507@gmail.com>
2025-03-13 19:56:13 +0700

    cap nhat login

---
+++ FrontEnd/PublicUI/Trangchu/header.php
@@ -0,0 +1,10 @@
+<header class="bg-blue-500 text-white p-4 flex justify-between items-center">
+    <h1 class="text-2xl font-bold">Tên Trang Web</h1>
+    <input type="text" placeholder="Tìm kiếm sản phẩm..." class="p-2 rounded text-black">
+    <div class="flex space-x-4">
+        <a href="index.php?page=signup" class="hover:underline">Đăng ký</a>
+        <a href="index.php?page=login" class="hover:underline">Đăng nhập</a>
+        <a href="#"><img src="icon-profile.svg" alt="Thông tin cá nhân" class="w-6 h-6"></a>
+        <a href="#"><img src="icon-cart.svg" alt="Giỏ hàng" class="w-6 h-6"></a>
+    </div>
+</header>
