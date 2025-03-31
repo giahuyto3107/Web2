6b5562328391d46a3dae004c470c22004163e0f9
BaoTheParo <tanthaibao123@gmail.com>
2025-03-31 08:17:47 +0700

    update chuc vu nhung chua menuphanquyen T-T

---
+++ FrontEnd/AdminUI/quanlichucvu/xemchucvu.php
@@ -0,0 +1,29 @@
+<dialog data-modal id="view-modal">
+    <div class="modal-header">
+        <h2>Chi tiết Chức Vụ</h2>
+        <button class="modal-close" data-id="view-modal">
+            <i class="fa fa-times" style="font-size: 1.5rem; height: 1.5rem"></i>
+        </button>
+    </div>
+    <div class="view-container">
+        <div class="view-content">
+            <span>ID</span>
+            <p id="modal-view-role-id">N/A</p>
+        </div>
+        <div class="view-content">
+            <span>Tên chức vụ</span>
+            <p id="modal-view-name">N/A</p>
+        </div>
+        <div class="view-content">
+            <span>Mô tả</span>
+            <p id="modal-view-description">N/A</p>
+        </div>
+        <div class="view-content">
+            <span>Trạng thái</span>
+            <p id="modal-view-status">N/A</p>
+        </div>
+        <div class="modal-buttons">
+            <button class="close" id="view-close-button">Đóng</button>
+        </div>
+    </div>
+</dialog>
