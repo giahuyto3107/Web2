74e03272964039a8778d68285f536c6e399b5332
BaoTheParo <tanthaibao123@gmail.com>
2025-04-15 11:21:42 +0700

    update css

--- BackEnd/Config/config.php
+++ BackEnd/Config/config.php
@@ -4,7 +4,7 @@ $servername = "localhost";
 $username = "root"; // Thay bằng username của bạn
 $password = "1234"; // Thay bằng password của bạn
 $dbname = "web2_sql";
-$port = "3305";
+$port = "3306";
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username,$password , $dbname, $port);
